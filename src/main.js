b60c756cfb7f18643245cd3188f6fd334f481968
Yangtian6008 <yangtian9897@163.com>
2020-10-12 18:13:26 +0800

    sec_change

--- src/main.js
+++ src/main.js
@@ -7,3 +7,15 @@ import {name,age} from './js/info.js';
 console.log(name);
 
 require('./css/one.css')
+import Vue from 'Vue'
+var vm=new Vue({
+    el:"#app",
+    data:{
+    msg:'学习使用webpack',
+    },
+    methods:{
+    btnclick(){
+    this.msg='寻找webpack 包装文件……'
+    }
+    },
+    })
